4e862175632f7ee689b7d5d2203caa17768878df
omkarvelankar <omkarvelankar@gmail.com>
2019-10-11 10:36:02 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-dream-app';
+  message = 'Using AngularJS, developers can create HTML-like elements and attributes that define the behavior of presentation components. These directives “let you invent new HTML syntax';
+  
+  myarray = ['omkar', 'Amar', 'Ankush', 'Aryan'];
+
+}
