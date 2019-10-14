77e8e304423e350d7297a009c235948ae03467a0
omkarvelankar <omkarvelankar@gmail.com>
2019-10-14 11:20:56 +0530

    NGSTYLE

--- src/app/Component/structural/structural.component.ts
+++ src/app/Component/structural/structural.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuralComponent implements OnInit {
 
-  constructor() { }
+  students: any;
+  constructor() {
+    this.students = [
+      {rollno: 10, name: 'omkar', marks: 85},
+      {rollno: 10, name: 'suraj', marks: 85},
+      {rollno: 10, name: 'amol', marks: 85},
+      {rollno: 10, name: 'ali', marks: 85},
+      {rollno: 10, name: 'abhay', marks: 85}, ];
+  }
 
-  ngOnInit() {
+
+
+ngOnInit() {
   }
 
 }
+
+
